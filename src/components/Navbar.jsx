6a7ae8b5e271ba2cc1fdaf71c2e800089a16e90f
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-11-23 14:18:46 +0600

    modified navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,13 @@ const Navbar = () => {
   return (
     <nav className="py-2 flex items-center w-10/12 mx-auto justify-between">
       <Image src="/images/Logo.png" alt="logo" width={80} height={80} />
-      <div className="flex justify-between items-center gap-18 text-orange-600">
+      <div className="flex justify-between items-center gap-12 text-orange-600">
         <Link href="/">Home</Link>
         <Link href="/recipes">Recipes</Link>
         <Link href="/addRecipes">Add Recipes</Link>
         {/* <Link href="/manageRecipes">Manage Recipes</Link> */}
-        <Button className={`rounded-full`}>Log In</Button>
+        <Link href='/login'><Button className={`rounded-full`}>Log In</Button></Link>
+        <Link href='/register'><Button className={`rounded-full`}>Register</Button></Link>
       </div>
     </nav>
   );
