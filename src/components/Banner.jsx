3d549a7ad2cf90724c2f24eaeb2cd0201ec38011
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-11-25 16:10:59 +0600

    banner responsiveness added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,17 +5,17 @@ import Link from 'next/link';
 
 const Banner = () => {
     return (
-        <div className='flex justify-between items-center w-10/12 mx-auto mt-20 gap-36'>
+        <div className='flex flex-col xl:flex-row justify-between items-center w-10/12 mx-auto mt-20 xl:gap-36 gap-12'>
             {/* banner content */}
-            <div>
-                <h1 className='text-7xl font-extrabold mb-5'>Your Kitchen <span className='text-orange-600'>Companion</span> <br /> for Every <span className='text-orange-600'>Recipe</span></h1>
-                <p className='text-xl text-nowrap mb-5'>Explore curated recipes, read details, and manage your own cooking collection.</p>
+            <div className='flex flex-col xl:items-start items-center flex-1'>
+                <h1 className='xl:text-7xl text-4xl text-center xl:text-left font-extrabold mb-5'>Your Kitchen <span className='text-orange-600'>Companion</span> <br /> for Every <span className='text-orange-600'>Recipe</span></h1>
+                <p className='text-xl xl:text-nowrap text-center xl:text-left mb-5'>Explore curated recipes, read details, and manage your own cooking collection.</p>
                 <Link href='/recipes'><Button className={`rounded-xl`}>Explore Recipes</Button></Link>
             </div>
 
             {/* banner image  */}
-            <div>
-                <Image src="/images/BannerImage.png" alt="logo" width={800} height={800} />
+            <div className='flex-1'>
+                <Image src="/images/BannerImage.png" alt="logo" width={800} height={800} className='mx-auto'/>
             </div>
         </div>
     );
